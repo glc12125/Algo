7c7bab77bde11f272988fcd22c5c9d29ada28845
glc12125 <Liangchuan Gu>
2018-09-17 18:28:06 +0100

    Update

--- lintcode/Rotated Digits.cpp
+++ lintcode/Rotated Digits.cpp
@@ -25,3 +25,41 @@ public:
         return result;
     }
 };
+
+// Recursize approach
+
+class Solution {
+private:
+    bool checkNum(int n) {
+        bool valid = false;
+        while(n>0){
+            switch(n%10){
+                case 2:
+                case 5:
+                case 6:
+                case 9:
+                    valid = true;
+                    break;
+                case 3:
+                case 4:
+                case 7:
+                    return false;
+            }
+            n /= 10;
+        }
+        return valid;
+    }
+public:
+    /**
+     * @param N: a positive number
+     * @return: how many numbers X from 1 to N are good
+     */
+    int rotatedDigits(int N) {
+        int result = 0;
+        for(int i = 1; i < N+1; ++i) {
+            if(checkNum(i)) ++result;
+        }
+        
+        return result;
+    }
+};
