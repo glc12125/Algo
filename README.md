52d7a5021c05eafe3a446f074ff01189fcdacbd7
glc12125 <Liangchuan Gu>
2018-09-19 14:35:40 +0100

    Update

--- README.md
+++ README.md
@@ -20,3 +20,4 @@
 |1|[Minimum Distance Between BST Nodes](https://github.com/glc12125/Algo/blob/master/lintcode/Minimum%20Distance%20Between%20BST%20Nodes.cpp)|Easy|C++|[Binary Search Tree]|Google|LintCode|
 |3|[Rotated Digits](https://github.com/glc12125/Algo/blob/master/lintcode/Rotated%20Digits.cpp)|Easy|C++|[String]|Google|LintCode|
 |4|[First Unique Character in a String](https://github.com/glc12125/Algo/blob/master/lintcode/First%20Unique%20Character%20in%20a%20String.cpp)|Easy|C++|[String]|Google, Amazon, Microsoft, Bloomberg|LintCode|
+|5|[Largest Triangle Area](https://github.com/glc12125/Algo/blob/master/lintcode/Largest%20Triangle%20Area.cpp)|Easy|C++|[]|Google|
---
+++ lintcode/Largest Triangle Area.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    double calculateArea(vector<int> A, vector<int> B, vector<int> C) {
+        return 0.5 * std::abs(A[0]*B[1] + B[0]*C[1] + C[0]*A[1] -
+                              A[1]*B[0] - B[1]*C[0] - C[1]*A[0]);
+    }
+public:
+    /**
+     * @param points: List[List[int]]
+     * @return: return a double
+     */
+    double largestTriangleArea(vector<vector<int>> &points) {
+        int N = points.size();
+        double area = 0;
+        for(int i = 0; i < N; ++i) {
+            for(int j = i + 1; j < N; ++j) {
+                for(int k = j + 1; k < N; ++k) {
+                    area = std::max(calculateArea(points[i], points[j], points[k]), area);
+                }
+            }
+        }
+        return area;
+    }
+};
