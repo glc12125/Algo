a488439ea1f666500672f37f7aea888339015e19
glc12125 <Liangchuan Gu>
2018-09-20 12:06:34 +0100

    Update

--- lintcode/Subarray Sum Equals K.cpp
+++ lintcode/Subarray Sum Equals K.cpp
@@ -1,3 +1,8 @@
+// The key is to avoid duplicated calculation. Create a hash table to store
+// the count of sums. After creating this hash table, check if there is any
+// thing like sum1 - sum2 = k, this means the sum of elements between the
+// ending point of sum1 and sum2 is k. And all of this can be done in a single
+// loop
 class Solution {
 public:
     /**
