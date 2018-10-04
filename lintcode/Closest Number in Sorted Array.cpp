0b39f1557592efdf3f0305816216c6f5f5a5c63f
glc12125 <Liangchuan Gu>
2018-10-04 16:40:52 +0100

    Update

--- lintcode/Closest Number in Sorted Array.cpp
+++ lintcode/Closest Number in Sorted Array.cpp
@@ -23,3 +23,29 @@ public:
         else return end;
     }
 };
+
+// A cleaner version
+class Solution {
+public:
+    /**
+     * @param A: an integer array sorted in ascending order
+     * @param target: An integer
+     * @return: an integer
+     */
+    int closestNumber(vector<int> &A, int target) {
+        int size = A.size();
+        if(size == 0) return -1;
+        int start = 0;
+        int end = size - 1;
+        int mid;
+        while (start + 1 < end) {
+            mid = (start + end) / 2;
+            if(A[mid] == target) return mid;
+            if(A[mid] > target) end = mid;
+            else start = mid;
+        }
+
+        if(target - A[start] <= A[end] - target) return start;
+        else return end;
+    }
+};
