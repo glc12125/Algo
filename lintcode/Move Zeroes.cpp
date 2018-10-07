8973b9cdf66fc78847f51d0da22e2333188dda31
glc12125 <Liangchuan Gu>
2018-10-07 11:37:23 +0100

    Update

--- lintcode/Move Zeroes.cpp
+++ lintcode/Move Zeroes.cpp
@@ -35,11 +35,38 @@ public:
             if(nums[targetPosition] != 0) {
                 nums[fillPosition] = nums[targetPosition];
                 ++fillPosition;
-            } 
+            }
             ++targetPosition;
         }
         while(fillPosition < size) {
             nums[fillPosition++] = 0;
+        }
+    }
+};
+
+// Minimize write operations
+class Solution {
+public:
+    /**
+     * @param nums: an integer array
+     * @return: nothing
+     */
+    void moveZeroes(vector<int> &nums) {
+        int size = nums.size();
+        int fillPosition = 0;
+        int targetPosition = 0;
+        while(targetPosition < size) {
+            if(nums[targetPosition] != 0) {
+                if(nums[fillPosition] != nums[targetPosition])
+                    nums[fillPosition] = nums[targetPosition];
+                ++fillPosition;
+            }
+            ++targetPosition;
+        }
+        while(fillPosition < size) {
+            if(nums[fillPosition] != 0)
+                nums[fillPosition] = 0;
+            ++fillPosition;
         }
     }
 };
