eb844e74ccdc53ebf391a72f82cdf0ac5793ff5e
glc12125 <Liangchuan Gu>
2018-10-07 18:28:59 +0100

    Update

--- lintcode/Sort Colors II.cpp
+++ lintcode/Sort Colors II.cpp
@@ -19,3 +19,39 @@ public:
         }
     }
 };
+
+// Without extra space
+class Solution {
+private:
+    void select(std::vector<int>& colors, int start, int end, int colorFrom, int colorTo) {
+        if(start >= end) return;
+        if(colorFrom >= colorTo) return;
+        int i = start;
+        int j = end;
+        int target = (colorTo + colorFrom) / 2;
+        while(i <= j) {
+            while(i <= j && colors[i] <= target) ++i;
+            while(i <= j && colors[j] > target) --j;
+            if(i <= j) {
+                int temp = colors[i];
+                colors[i] = colors[j];
+                colors[j] = temp;
+                ++i;
+                --j;
+            }
+        }
+        select(colors, start, j, colorFrom, target);
+        select(colors, i, end, target + 1, colorTo);
+    }
+public:
+    /**
+     * @param colors: A list of integer
+     * @param k: An integer
+     * @return: nothing
+     */
+    void sortColors2(vector<int> &colors, int k) {
+        int len = colors.size();
+        if(len <= 1) return;
+        select(colors, 0, len - 1, 1, k);
+    }
+};
