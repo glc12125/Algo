aaf38063c493906df8a4395c962ca5197e4ec06f
glc12125 <Liangchuan Gu>
2018-10-10 15:13:28 +0100

    Update

--- lintcode/Smallest Rectangle Enclosing Black Pixels.cpp
+++ lintcode/Smallest Rectangle Enclosing Black Pixels.cpp
@@ -35,3 +35,67 @@ public:
         return (maxX - minX + 1) * (maxY - minY + 1);
     }
 };
+
+// Using binary search
+class Solution {
+private:
+    typedef bool(*CheckFuncPtr)(vector<vector<char>>&, int);
+    
+    static bool checkColumn(vector<vector<char>>& image, int col) {
+        for (int i = 0; i < image.size(); i++) {
+            if (image[i][col] == '1') {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    static bool checkRow(vector<vector<char>>& image, int row) {
+        for (int j = 0; j < image[0].size(); j++) {
+            if (image[row][j] == '1') {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    void search(vector<vector<char>>& image, int start, int end, int& finalStart, int& finalEnd, CheckFuncPtr check, bool smallerSide) {
+        finalStart = start;
+        finalEnd = end;
+        int mid;
+        while (finalStart + 1 < finalEnd) {
+            mid = finalStart + (finalEnd - finalStart) / 2;
+            if ((*check)(image, mid)) {
+                smallerSide ? finalEnd = mid : finalStart = mid;
+            } else {
+                smallerSide ? finalStart = mid : finalEnd = mid;
+            }
+        }
+    }
+public:
+    int minArea(vector<vector<char>>& image, int x, int y) {
+        // Write your code here
+        int m = image.size();
+        if (m == 0)
+            return 0;
+        int n = image[0].size();
+        if (n == 0)
+            return 0;
+
+        int start;
+        int end;
+        search(image, y, n-1, start, end, &Solution::checkColumn, false);
+        int right = checkColumn(image, end) ? end : start;
+        
+        search(image, 0, y, start, end, &Solution::checkColumn, true);
+        int left = checkColumn(image, start) ? start : end;
+        
+        search(image, x, m-1, start, end, &Solution::checkRow, false);
+        int down = checkRow(image, end) ? end : start;
+        
+        search(image, 0, x, start, end, &Solution::checkRow, true);
+        int up = checkRow(image, start) ? start : end;
+        
+        return (right - left + 1) * (down - up + 1);
+    }
+};
