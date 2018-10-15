25585b055a46393c377392dce9cadd54a911ab35
glc12125 <Liangchuan Gu>
2018-10-15 14:42:37 +0100

    Update

--- lintcode/Sort Colors.cpp
+++ lintcode/Sort Colors.cpp
@@ -38,3 +38,38 @@ public:
         }
     }
 };
+
+// Three Pointers
+class Solution {
+private:
+    void swap(int& a, int&b) {
+        if(a != b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+public:
+    /**
+     * @param nums: A list of integer which is 0, 1 or 2
+     * @return: nothing
+     */
+    void sortColors(vector<int> &nums) {
+        int len = nums.size();
+        if(len == 0) return;
+        int pointerR = 0;
+        int pointerW = 0;
+        int pointerB = len - 1;
+        while(pointerW <= pointerB) {
+            if(nums[pointerW] == 0) {
+                swap(nums[pointerR], nums[pointerW]);
+                ++pointerR;
+                ++pointerW;
+            } else if (nums[pointerW] == 1) {++pointerW;}
+            else {
+                swap(nums[pointerW], nums[pointerB]);
+                --pointerB;
+            }
+        }
+    }
+};
