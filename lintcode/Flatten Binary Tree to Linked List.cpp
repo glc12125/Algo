fefc1baae09b87697e186e0989dda5aeb8c3d6a2
glc12125 <Liangchuan Gu>
2018-10-20 23:20:54 +0100

    Update

--- lintcode/Flatten Binary Tree to Linked List.cpp
+++ lintcode/Flatten Binary Tree to Linked List.cpp
@@ -11,6 +11,7 @@
  * }
  */
 
+// Traversal
 class Solution {
 private:
     TreeNode * m_last;
@@ -21,7 +22,7 @@ public:
      */
     void flatten(TreeNode * root) {
         if(root == nullptr) return;
-        
+
         if(m_last != nullptr) {
             m_last->left = nullptr;
             m_last->right = root;
@@ -30,5 +31,42 @@ public:
         auto right = root->right;
         flatten(root->left);
         flatten(right);
+    }
+};
+
+
+// Divide and conquer
+class Solution {
+private:
+    TreeNode * divideNConque(TreeNode * root) {
+        if(root == nullptr) return nullptr;
+
+        auto leftLast = divideNConque(root->left);
+        auto rightLast = divideNConque(root->right);
+
+        if(leftLast != nullptr) {
+            leftLast->right = root->right;
+            root->right = root->left;
+            root->left = nullptr;
+        }
+
+        if(rightLast != nullptr) {
+            return rightLast;
+        }
+
+        if(leftLast != nullptr) {
+            return leftLast;
+        }
+
+        return root;
+
+    }
+public:
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    void flatten(TreeNode * root) {
+        divideNConque(root);
     }
 };
