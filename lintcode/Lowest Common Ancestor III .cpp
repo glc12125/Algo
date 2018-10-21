39a68e88ce24d51285c9e7164f79b792ec4eee1a
glc12125 <Liangchuan Gu>
2018-10-21 12:40:25 +0100

    Update

--- lintcode/Lowest Common Ancestor III .cpp
+++ lintcode/Lowest Common Ancestor III .cpp
@@ -76,3 +76,60 @@ public:
         return result.m_lca;
     }
 };
+
+
+
+// Similar approach but cleaner
+class Solution {
+private:
+    struct ResultType{
+        bool m_foundA;
+        bool m_foundB;
+        TreeNode * m_node;
+
+        ResultType() : m_foundA(false), m_foundB(false), m_node(nullptr) {
+        }
+
+        ResultType(bool foundA, bool foundB, TreeNode* node) : m_foundA(foundA), m_foundB(foundB), m_node(node) {
+        }
+    };
+
+    ResultType findLca(TreeNode * root, TreeNode * A, TreeNode * B) {
+        if(root == nullptr) {
+            return ResultType(false, false, nullptr);
+        }
+
+        auto leftResult = findLca(root->left, A, B);
+        auto rightResult = findLca(root->right, A, B);
+
+        bool foundA = leftResult.m_foundA || rightResult.m_foundA || root->val == A->val;
+        bool foundB = leftResult.m_foundB || rightResult.m_foundB || root->val == B->val;
+
+        if(root->val == A->val || root->val == B->val) {
+            return ResultType(foundA, foundB, root);
+        }
+
+        if(leftResult.m_node != nullptr && rightResult.m_node != nullptr) {
+            return ResultType(foundA, foundB, root);
+        }
+        if(leftResult.m_node != nullptr ) {
+            return ResultType(foundA, foundB, leftResult.m_node);
+        }
+        if(rightResult.m_node != nullptr ) {
+            return ResultType(foundA, foundB, rightResult.m_node);
+        }
+
+    }
+public:
+    /*
+     * @param root: The root of the binary tree.
+     * @param A: A TreeNode
+     * @param B: A TreeNode
+     * @return: Return the LCA of the two nodes.
+     */
+    TreeNode * lowestCommonAncestor3(TreeNode * root, TreeNode * A, TreeNode * B) {
+        auto result = findLca(root, A, B);
+        if(result.m_foundA && result.m_foundB) return result.m_node;
+        else return nullptr;
+    }
+};
