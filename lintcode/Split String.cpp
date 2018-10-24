4e28f6f353f7d5391ac28e2584151ec7717e6800
glc12125 <Liangchuan Gu>
2018-10-24 16:51:35 +0100

    Update

--- lintcode/Split String.cpp
+++ lintcode/Split String.cpp
@@ -28,3 +28,34 @@ public:
         return results;
     }
 };
+
+// Combination based approach
+class Solution {
+private:
+    void dfs(const std::string& s, int index, std::vector<std::string>& splits, std::vector<std::vector<std::string> >& results) {
+        int len = s.size();
+        if(index >= len) {
+            results.push_back(splits);
+            return;
+        }
+
+        for(int i = 1; i < 3; ++i) {
+            if(index + i <= len) {
+                splits.push_back(s.substr(index, i));
+                dfs(s, index + i, splits, results);
+                splits.pop_back();
+            }
+        }
+    }
+public:
+    /*
+     * @param : a string to be split
+     * @return: all possible split string array
+     */
+    vector<vector<string>> splitString(string& s) {
+        std::vector<std::vector<std::string> > results;
+        std::vector<std::string> splits;
+        dfs(s, 0, splits, results);
+        return results;
+    }
+};
