542b1f939a79d62d1106b3c0cdc7c4705d202d7c
glc12125 <Liangchuan Gu>
2018-10-27 14:32:27 +0100

    Update

--- lintcode/String Permutation II.cpp
+++ lintcode/String Permutation II.cpp
@@ -32,3 +32,55 @@ public:
         return results;
     }
 };
+
+// Iterative approach
+class Solution {
+private:
+
+    bool nextPermutation(std::string& str) {
+        int firstDecreasingIndex = -1;
+        int len = str.size();
+        for(int i = len - 1; i > 0; --i) {
+            if(str[i] > str[i-1]) {
+                firstDecreasingIndex = i - 1;
+                break;
+            }
+        }
+
+        if(firstDecreasingIndex == -1) return false;
+
+        for(int i = len - 1; i > firstDecreasingIndex; --i) {
+            if(str[i] > str[firstDecreasingIndex]) {
+                swap(str[i], str[firstDecreasingIndex]);
+                break;
+            }
+        }
+        reverseAllFromIndex(str, firstDecreasingIndex + 1, len - 1);
+        return true;
+    }
+
+    void swap(char& a, char& b) {
+        char temp = a;
+        a = b;
+        b = temp;
+    }
+
+    void reverseAllFromIndex(std::string& str, int index, int end) {
+        for(int i = index, j = end; i < j; ++i, --j) {
+            swap(str[i], str[j]);
+        }
+    }
+public:
+
+
+    vector<string> stringPermutation2(string &str) {
+        std::vector<std::string> results;
+        std::string permutation;
+        std::sort(str.begin(), str.end());
+        results.push_back(str);
+
+        while(nextPermutation(str)) results.push_back(str);
+
+        return results;
+    }
+};
