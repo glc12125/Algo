618e0a46a8daef08caaaa8400d2cf0e447e02568
glc12125 <Liangchuan Gu>
2018-10-30 16:52:41 +0000

    Update

--- README.md
+++ README.md
@@ -216,3 +216,4 @@
 |196|[Load Balancer](https://github.com/glc12125/Algo/blob/master/lintcode/Load%20Balancer.cpp)|Medium|C++|[Hash Table, Array]|Google|LintCode|
 |197|[Merge K Sorted Arrays](https://github.com/glc12125/Algo/blob/master/lintcode/Merge%20K%20Sorted%20Arrays.cpp)|Medium|C++|[Heap, Priority Queue]||LintCode|
 |198|[Rehashing](https://github.com/glc12125/Algo/blob/master/lintcode/Rehashing.cpp)|Medium|C++|[Hash Table]||LintCode|
+|199|[Longest Consecutive Sequence](https://github.com/glc12125/Algo/blob/master/lintcode/Longest%20Consecutive%20Sequence.cpp)|Medium|C++|[Array]|Facebook, Google|LintCode|
---
+++ lintcode/Longest Consecutive Sequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    /**
+     * @param num: A list of integers
+     * @return: An integer
+     */
+    int longestConsecutive(vector<int> &num) {
+        std::unordered_set<int> uniqueNums;
+        for(auto i : num) uniqueNums.insert(i);
+
+        int longest = 1;
+        for(auto i : num) {
+            int smaller = i - 1;
+            while(uniqueNums.count(smaller)) {
+                uniqueNums.erase(smaller);
+                --smaller;
+            }
+            int bigger = i + 1;
+            while(uniqueNums.count(bigger)) {
+                uniqueNums.erase(bigger);
+                ++bigger;
+            }
+
+            longest = std::max(longest, bigger - smaller - 1);
+        }
+
+        return longest;
+    }
+};
