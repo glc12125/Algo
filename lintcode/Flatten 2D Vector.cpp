c22d5c124a22561c1a307121f1dfe238d7b29ad7
glc12125 <Liangchuan Gu>
2018-10-30 14:38:08 +0000

    Update

--- lintcode/Flatten 2D Vector.cpp
+++ lintcode/Flatten 2D Vector.cpp
@@ -32,8 +32,49 @@ public:
     }
 };
 
+
+// Use Stacks
+class Vector2D {
+private:
+    std::stack<std::vector<int> > m_mainContainer;
+    std::stack<int> m_workSpace;
+
+    void addVector2DtoMain(vector<vector<int>>& vec2d) {
+        int len = vec2d.size();
+        for(int i = len - 1; i > -1; --i) {
+            std::vector<int> vec(vec2d[i]);
+            if(vec.empty()) continue;
+            std::reverse(vec.begin(), vec.end());
+            m_mainContainer.push(vec);
+        }
+    }
+
+    void addVectorToBuffer() {
+        if(m_mainContainer.empty()) return;
+        auto line = m_mainContainer.top();
+        m_mainContainer.pop();
+        for(auto i : line) m_workSpace.push(i);
+    }
+public:
+    Vector2D(vector<vector<int>>& vec2d) {
+        addVector2DtoMain(vec2d);
+    }
+
+    int next() {
+        auto result =  m_workSpace.top();
+        m_workSpace.pop();
+        return result;
+    }
+
+    bool hasNext() {
+        if(m_workSpace.empty() && !m_mainContainer.empty()) addVectorToBuffer();
+        return !m_workSpace.empty();
+    }
+};
+
 /**
  * Your Vector2D object will be instantiated and called as such:
  * Vector2D i(vec2d);
  * while (i.hasNext()) cout << i.next();
  */
+
