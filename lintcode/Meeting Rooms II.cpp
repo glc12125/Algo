948ee8893c1f33dae6612ff64ed86a69826689ca
glc12125 <Liangchuan Gu>
2018-11-09 12:16:25 +0000

    Update

--- lintcode/Meeting Rooms II.cpp
+++ lintcode/Meeting Rooms II.cpp
@@ -9,6 +9,7 @@
  * }
  */
 
+// Time O(NlogN)
 class Solution {
 public:
 
@@ -30,6 +31,54 @@ public:
         int rooms = 0;
         for(const auto& i : typeSequence) {
             rooms += i.second;
+            maxRooms = std::max(rooms, maxRooms);
+        }
+
+        return maxRooms;
+    }
+};
+
+// Also Sweep line but using vector + custom comparator. Time is also O(NlogN)
+class Solution {
+private:
+    struct Time{
+        int m_time;
+        int m_type;
+        Time() = default;
+        Time(int t, int type) : m_time(t), m_type(type) {}
+    };
+
+    struct TimeCompare{
+        bool operator()(const Time& t1, const Time& t2) const {
+            if(t1.m_time == t2.m_time) return t1.m_type < t2.m_type; // make start type 0 first
+            else return t1.m_time < t2.m_time;
+        }
+    };
+public:
+
+    /**
+     * @param intervals: an array of meeting time intervals
+     * @return: the minimum number of conference rooms required
+     */
+    int minMeetingRooms(vector<Interval> &intervals) {
+        int len = intervals.size();
+        if(len == 0) return 0;
+
+        vector<Time> typeSequence;
+        for(const auto& i : intervals) {
+            typeSequence.push_back(Time(i.start, 0));
+            typeSequence.push_back(Time(i.end, 1));
+        }
+
+        std::sort(typeSequence.begin(), typeSequence.end(), TimeCompare());
+        int maxRooms = 0;
+        int rooms = 0;
+        for(const auto& i : typeSequence) {
+            if(i.m_type == 0) {
+                ++rooms;
+            } else {
+                --rooms;
+            }
             maxRooms = std::max(rooms, maxRooms);
         }
 
