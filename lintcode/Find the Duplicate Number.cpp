570ce218ee2909ac75ec05e0c57b05675997fbda
glc12125 <Liangchuan Gu>
2018-11-09 14:35:18 +0000

    Update

---
+++ lintcode/Find the Duplicate Number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    int getSTE(const vector<int>& nums, int mid) {
+        int cnt = 0;
+        for(auto num : nums) {
+            if(num <= mid) ++cnt;
+        }
+        return cnt;
+    }
+public:
+    /**
+     * @param nums: an array containing n + 1 integers which is between 1 and n
+     * @return: the duplicate one
+     */
+    int findDuplicate(vector<int> &nums) {
+        int len = nums.size();
+        int l = 0;
+        int r = len - 1;
+        while(l + 1 < r) {
+            int mid = l + (r - l) / 2;
+            if(getSTE(nums, mid) <= mid) {
+                l = mid;
+            } else r = mid;
+        }
+
+        if(getSTE(nums, l) <= l) return r;
+        return l;
+    }
+};
