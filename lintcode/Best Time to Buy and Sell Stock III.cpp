e48c02e112ccc8762f8ab3a03676be1c1d55a709
glc12125 <Liangchuan Gu>
2018-11-15 22:42:25 +0000

    Update

---
+++ lintcode/Best Time to Buy and Sell Stock III.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    /**
+     * @param prices: Given an integer array
+     * @return: Maximum profit
+     */
+    int maxProfit(vector<int> &prices) {
+        int len = prices.size();
+
+        vector<vector<int> > dp(len + 1, vector<int>(5, INT_MIN));
+
+        dp[0][0] = 0;
+        //for(int i = 1; i < 5; ++i) dp[0][i] = INT_MIN;
+
+        for(int i = 1; i <= len; ++i) {
+            for(int j = 0; j < 5; ++j) {
+                if(j&1) { // odd index, even days 2, 4, with holdings
+                    dp[i][j] = dp[i-1][j-1];
+                    if(i-2 >= 0 && dp[i-1][j] != INT_MIN) dp[i][j] = std::max(dp[i-1][j] + prices[i-1] - prices[i-2], dp[i][j]);
+                } else { // even index, odd days 1, 3, 5 no holdings
+                    dp[i][j] = dp[i-1][j];
+                    if(i-2 >= 0 && j > 0 && dp[i-1][j-1] != INT_MIN) dp[i][j] = std::max(dp[i][j], dp[i-1][j-1] + prices[i-1] - prices[i-2]);
+                }
+            }
+        }
+
+        int maxP = INT_MIN;
+        for(int i = 0; i < 5;) {
+            maxP = std::max(maxP, dp[len][i]);
+            i += 2;
+        }
+        return maxP;
+    }
+};
