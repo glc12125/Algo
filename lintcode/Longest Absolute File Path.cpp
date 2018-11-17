07c97c3b671dd24061657adc993a3ec7efd7e08f
glc12125 <Liangchuan Gu>
2018-11-17 15:47:26 +0000

    Update

--- lintcode/Longest Absolute File Path.cpp
+++ lintcode/Longest Absolute File Path.cpp
@@ -1,5 +1,63 @@
 class Solution {
 private:
+    void splitBy(const string& str, vector<string>& container, const string& delim) {
+        /*stringstream ss(s);
+        string token;
+        while(getline(ss, token, delim)) {
+            container.push_back(token);
+        }*/
+        std::size_t current, previous = 0;
+        current = str.find(delim);
+        while (current != std::string::npos) {
+            container.push_back(str.substr(previous, current - previous));
+            previous = current + 2;
+            current = str.find(delim, previous);
+        }
+        container.push_back(str.substr(previous, current - previous));
+    }
+
+    int findLastOf(const string& str, const string& target) {
+        int prev = -1;
+        int curr = str.find(target);
+        while(curr >= 0) {
+            prev = curr;
+            curr = str.find(target, prev + target.size());
+        }
+        return prev;
+    }
+public:
+    /**
+     * @param input: an abstract file system
+     * @return: return the length of the longest absolute path to file
+     */
+    int lengthLongestPath(string &input) {
+        int len = input.size();
+        if(len == 0) return 0;
+
+        vector<string> lines;
+        splitBy(input, lines, "\\n"); // Note that '\' is not used as escaping char
+
+        vector<int> levelLength(len + 1, 0);
+        int maxLen = 0;
+        for(const auto& line : lines) {
+            auto lastIndex = findLastOf(line, "\\t");
+            int offSet = lastIndex == -1 ? 0 : lastIndex + 2;
+            int level = lastIndex == -1 ? 1 : lastIndex / 2 + 2;
+            int len = line.size() - offSet;
+            if(line.find('.') != string::npos) {
+                maxLen = std::max(maxLen, levelLength[level-1] + len);
+            } else {
+                levelLength[level] = levelLength[level-1] + len + 1;
+            }
+        }
+
+        return maxLen;
+    }
+};
+
+// Do not use extra O(N)(N is the number of \n) space to store parts of path
+class Solution {
+private:
     int countTab(const std::string& str)
     {
         int res = 0;
@@ -10,7 +68,7 @@ private:
         }
         return res;
     }
-    
+
     bool isFile(const std::string& str)
     {
         return str.find('.') != std::string::npos;
@@ -22,11 +80,11 @@ public:
      * @return: return the length of the longest absolute path to file
      */
     int lengthLongestPath(string &input) {
-        
+
         int len = 0;
         int start = 0;
         input.append("\\n");
-        
+
         std::vector<int> tabLenMap(100, 0);
         int pos = input.find("\\n");
         int maxLen = 0;
@@ -41,7 +99,7 @@ public:
             }
             pos = input.find("\\n", start);
         }
-        
+
         return maxLen;
     }
 };
