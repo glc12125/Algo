26783ae3e518babf7296e3d490ea01471dfc42e2
glc12125 <Liangchuan Gu>
2018-11-17 23:01:36 +0000

    Update

--- lintcode/Wildcard Matching.cpp
+++ lintcode/Wildcard Matching.cpp
@@ -54,3 +54,49 @@ public:
         return match(s, 0, p, 0, mem);
     }
 };
+
+//DP
+class Solution {
+public:
+    /**
+     * @param s: A string
+     * @param p: A string includes "?" and "*"
+     * @return: is Match?
+     */
+    bool isMatch(string &s, string &p) {
+        if(p.size() == 0) return false;
+        int m = s.size();
+        int n = p.size();
+
+        vector<vector<bool>> dp(m+1, vector<bool>(n+1));
+
+        for(int i = 0; i <= m; ++i) {
+            for(int j = 0; j <= n; ++j) {
+
+                if(i == 0 && j == 0) {
+                    dp[i][j] = true;
+                    continue;
+                }
+
+                if(j == 0) {
+                    dp[i][j] =false;
+                    continue;
+                }
+
+                dp[i][j] = false;
+                if(p[j-1] != '*') {
+                    if(i > 0 && (p[j-1] == '?' || s[i-1] == p[j-1])) {
+                        dp[i][j] = dp[i-1][j-1];
+                    }
+                } else {
+                      dp[i][j] = dp[i][j-1];
+                      if(i > 0) {
+                          dp[i][j] = dp[i][j] || dp[i-1][j];
+                      }
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
