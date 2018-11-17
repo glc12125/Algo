7338964e57f54e83864e58c3c394be641ed1e639
glc12125 <Liangchuan Gu>
2018-11-17 14:33:55 +0000

    Update

--- lintcode/Burst Balloons.cpp
+++ lintcode/Burst Balloons.cpp
@@ -32,3 +32,41 @@ public:
         return memoBurst(expandedNums, 1, len, dp, visited);
     }
 };
+
+
+// DP, time O(N^3), Space O(N^2)
+int maxCoins(vector<int> &nums) {
+        int n = nums.size();
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+
+        vector<int> paddedNums(n + 2, 1);
+        for(int i = 1; i <= n; ++i) {
+            paddedNums[i] = nums[i - 1];
+        }
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+
+        for(int i = 0; i <= n; ++i) {
+            dp[i][i+1] = 0;
+        }
+
+        for(int len = 1; len <= n; ++len) {
+            // i [0, ... , n - len]
+            for(int i = 0; i <= n - len; ++i) {
+                int j = i + len + 1;
+                dp[i][j] = 0;
+                for(int k = i + 1; k < j; ++k) {
+                    dp[i][j] = std::max(dp[i][j], dp[i][k] + dp[k][j] + paddedNums[i] * paddedNums[k] * paddedNums[j]);
+                }
+            }
+        }
+
+        /*for(int i = 0; i <= n + 1; ++i) {
+            std::cout << "\n";
+            for(int j = 0; j <= n + 1; ++j) {
+                std::cout << dp[i][j] << " ";
+            }
+        }*/
+
+        return dp[0][n+1];
+    }
--- lintcode/Pow(x, n).cpp
+++ lintcode/Pow(x, n).cpp
@@ -20,3 +20,36 @@ public:
         return n % 2 ? x * halfResult * halfResult : halfResult * halfResult;
     }
 };
+
+// Iterative
+class Solution {
+public:
+    /*
+     * @param x: the base number
+     * @param n: the power number
+     * @return: the result
+     */
+    double myPow(double x, int n) {
+        bool isNegative = false;
+        if (n < 0) {
+            x = 1 / x;
+            isNegative = true;
+            n = - (n + 1); // handle integer overflow
+        }
+        double ans = 1, temp = x;
+        while (n != 0) {
+            if (n % 2 == 1) {
+                ans *= temp;
+            }
+            temp *= temp;
+            n /= 2;
+        }
+
+        if (isNegative) {
+            ans *= x;
+        }
+
+        return ans;
+
+    }
+};
