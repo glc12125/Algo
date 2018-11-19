23fc5423a4ccf035cb0d0d9f6a5fe4ee598badbd
glc12125 <Liangchuan Gu>
2018-11-19 20:26:28 +0000

    Update

--- lintcode/H-Index.cpp
+++ lintcode/H-Index.cpp
@@ -1,3 +1,4 @@
+// Time O(NlogN), space O(1)
 class Solution {
 public:
     /**
@@ -18,5 +19,41 @@ public:
             } else break;
         }
         return hIndex == -1 ? 0 : len - hIndex;
+    }
+};
+
+//Time O(N), space O(N)
+class Solution {
+public:
+    /**
+     * @param citations: a list of integers
+     * @return: return a integer
+     */
+    int hIndex(vector<int> &citations) {
+        int n = citations.size();
+        vector<int> count(n + 1, 0);
+
+        for(int i = 0; i < n; i++)
+        {
+            if(citations[i] >= n) {
+                count[n]++;
+            } else {
+                count[citations[i]]++;
+            };
+        }
+
+        if(count[n] >= n) {
+            return n;
+        }
+
+        for(int i = n - 1; i >= 0; i--)
+        {
+            count[i] += count[i+1];
+            if(count[i] >= i) {
+                return i;
+            }
+        }
+
+        return 0;
     }
 };
