7d826fee5f176b36fa1bc0760bb4d87f7bdcf7aa
glc12125 <Liangchuan Gu>
2018-11-28 12:07:31 +0000

    Update

--- lintcode/Single Number IV.cpp
+++ lintcode/Single Number IV.cpp
@@ -14,3 +14,39 @@ public:
         return -1;
     }
 };
+
+// Binary Search, time O(N)
+class Solution {
+public:
+    /**
+     * @param nums: The number array
+     * @return: Return the single number
+     */
+    int getSingleNumber(vector<int> &nums) {
+        int len = nums.size();
+        if(len == 1) return nums[0];
+
+        int left = 0;
+        int right = len - 1;
+        while(left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if(mid % 2 == 0) {
+                // First enter here
+                if(nums[mid-1] == nums[mid]) {
+                    right = mid;
+                } else {
+                    left = mid;
+                }
+            } else {
+                if(nums[mid-1] == nums[mid]) {
+                    left = mid;
+                } else {
+                    right = mid;
+                }
+            }
+        }
+
+        if(nums[left] == nums[left - 1]) return nums[right];
+        else return nums[left];
+    }
+};
