f9d8be65a83218eed1d556dfcc980e4f9a7de0a8
glc12125 <Liangchuan Gu>
2018-11-29 11:53:12 +0000

    Update

--- lintcode/Tree Longest Path With Same Value.cpp
+++ lintcode/Tree Longest Path With Same Value.cpp
@@ -57,3 +57,54 @@ public:
         return divideAndConquer(A, tree, 1).m_max - 1;
     }
 };
+
+// DFS
+class Solution {
+public:
+    /*
+     * @param : as indicated in the description
+     * @param : as indicated in the description
+     * @return: Return the number of edges on the longest path with same value.
+     */
+    int LongestPathWithSameValue(vector<int> &A, vector<int> &E) {
+        // write your code here
+        int n = A.size();
+        if (n == 0 || n == 1) {
+            return 0;
+        }
+        edges = vector<unordered_set<int>>(1 + n);
+        for (int j = 0; j < n - 1; ++j) {
+            edges[E[2 * j]].insert(E[2 * j + 1]);
+            edges[E[2 * j + 1]].insert(E[2 * j]);
+        }
+
+        maxLen = 0;
+        dfs(A, 1, 0);
+        return maxLen - 1;
+    }
+
+private:
+    int maxLen;
+    vector<unordered_set<int>> edges;
+    int dfs(vector<int> &A, int start, int prev) {
+        int currMax1 = 0, currMax2 = 0;
+        for (int next : edges[start]) {
+            if (next == prev) {
+                continue;
+            }
+            int len = dfs(A, next, start);
+            if (A[start - 1] == A[next - 1]) {
+                if (len > currMax1) {
+                    currMax2 = currMax1;
+                    currMax1 = len;
+                }
+                else if (len > currMax2) {
+                    currMax2 = len;
+                }
+            }
+        }
+
+        maxLen = max(maxLen, currMax1 + currMax2 + 1);
+        return currMax1 + 1;
+    }
+};
