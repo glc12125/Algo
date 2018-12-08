104556ae89f3ff07183d2db3f06195ce6d6b3cd4
glc12125 <Liangchuan Gu>
2018-12-08 16:28:53 +0000

    Update

--- lintcode/Fermat Point Of Graphs.cpp
+++ lintcode/Fermat Point Of Graphs.cpp
@@ -1,52 +1,81 @@
 /*
 Tree dynamic programming
-dp[i] represents the aggregated distance from i to all the substree nodes with i as the root，dp[i] = sum(dp[j] + np[j] * d(i, j))
-np[i] represents the subtree node number with i as the root，np[i] = sum(np[j]) + 1。
+dp[i] represents the aggregated distance from i to all the substree nodes with i as the root，dp[i] = sum(dp[j] + size[j] * d(i, j))
+size[i] represents the subtree node number with i as the size[i] = sum(size[j]) + 1。
+
+This question is a weighted version of "Sums of Distances in Tree"
+With a similar solution。we choose a node as root and calculate the aggregated distance for all the
+other nodes. We also need to calculate the substree size for each node. The transition equation is：
+
+dp[i] = dp[j] - size[i] * w(i, j) + (n - size[i]) * w(i, j), where j is i's parent
+the distance between subtree nodes and i(as root) is decrease by size[i] * w(i, j) compared to that with j as root.
+The distance between all the other nodes that are not subtree with i as root increases by (n - size[i]) * w(i, j)
+where n is the number of nodes，and w(i, j) means the weight between i and j。We do dfs from top to bottom.
+The space and time complexity are both O(n).
 */
 class Solution {
-    void dfs1(int x, int f, vector<vector<pair<int, int>>> &g, vector<int> &np, vector<long long> &dp) {
-        np[x] = 1;
-        dp[x] = 0;
-        for (int i = 0; i < g[x].size(); i++) {
-            int y = g[x][i].first;
-            if (y == f) {
-                continue;
-            }
-            dfs1(y, x, g, np, dp);
-            np[x] += np[y];
-            dp[x] += dp[y] + (long long)g[x][i].second * np[y];
+class Solution {
+public:
+    /**
+    * @param x: The end points set of edges
+    * @param y: The end points set of edges
+    * @param d: The length of edges
+    * @return: Return the index of the fermat point
+    */
+    int getFermatPoint(vector<int> &x, vector<int> &y, vector<int> &d)
+    {
+        //acyclic connected graph, V = E + 1
+        int n = x.size() + 1;
+        vector<vector<pair<int, int>>> g(n + 1);
+        for (int i = 0; i < x.size(); ++i)
+        {
+            int u = x[i], v = y[i], w = d[i];
+            g[u].emplace_back(v, w);
+            g[v].emplace_back(u, w);
         }
+        vector<long long> dp(n + 1, 0);
+        vector<int> sz(n + 1, 0);
+        countSubTree(0, 1, g, dp, sz);
+        long long minDist = dp[1];
+        int idx = 1;
+        findPoint(0, 1, g, sz, dp[1], idx, minDist);
+        return idx;
     }
-    int dfs2(int x, int f, long long sum, vector<vector<pair<int, int>>> &g, vector<int> &np, vector<long long> &dp, long long &ans, int &idx, int n) {
-        for (int i = 0; i < g[x].size(); i++) {
-            int y = g[x][i].first;
-            if (y == f) {
-                continue;
+
+private:
+    void countSubTree(int from, int to, vector<vector<pair<int, int>>>& g, vector<long long>& dp, vector<int>& sz)
+    {
+        sz[to] = 1;
+        for (const auto& adj : g[to])
+        {
+            int v = adj.first;
+            long long w = adj.second;
+            if (v != from)
+            {
+                countSubTree(to, v, g, dp, sz);
+                sz[to] += sz[v];
+                dp[to] += dp[v] + sz[v] * w;
             }
-            long long nextSum = dp[y] + (sum - dp[y] - (long long)np[y] * g[x][i].second) + (long long)(n - np[y]) * g[x][i].second;
-            if (nextSum < ans || (nextSum == ans && x < idx)) {
-                ans = nextSum;
-                idx = y;
-            }
-            dfs2(y, x, nextSum, g, np, dp, ans, idx, n);
         }
     }
 
-public:
-    int getFermatPoint(vector<int> &x, vector<int> &y, vector<int> &d) {
-        // Write your code here
-        int n = x.size() + 1;
-        vector<vector<pair<int, int>>> g(n + 1);
-        for (int i = 0; i < x.size(); i++) {
-            g[x[i]].push_back(make_pair(y[i], d[i]));
-            g[y[i]].push_back(make_pair(x[i], d[i]));
+    void findPoint(int from, int to, vector<vector<pair<int, int>>>& g, vector<int>& sz, long long dist, int& res, long long& minDist)
+    {
+        int n = sz.size() - 1;
+        for (const auto& adj : g[to])
+        {
+            int v = adj.first;
+            long long w = adj.second;
+            if (v != from)
+            {
+                long long currDist = dist - sz[v] * w + (n - sz[v]) * w;
+                if (currDist < minDist)
+                {
+                    minDist = currDist;
+                    res = v;
+                }
+                findPoint(to, v, g, sz, currDist, res, minDist);
+            }
         }
-        vector<int> np(n + 1, 0);
-        vector<long long> dp(n + 1, 0);
-        dfs1(1, 0, g, np, dp);
-        long long ans = dp[1];
-        int idx = 1;
-        dfs2(1, 0, dp[1], g, np, dp, ans, idx, n);
-        return idx;
     }
 };
--- lintcode/Implement strStr().cpp
+++ lintcode/Implement strStr().cpp
@@ -1,3 +1,4 @@
+// Time O(M+N)
 class Solution {
 private:
     int m_BASE = 1e6;
@@ -39,5 +40,101 @@ public:
         }
 
         return -1;
+    }
+};
+
+// KMP, time O(M+N)
+class Solution {
+private:
+    // Fills lps[] for given patttern pat[0..M-1]
+    void computeLPSArray(const char* pat, int M, int* lps)
+    {
+        // length of the previous longest prefix suffix
+        int len = 0;
+
+        lps[0] = 0; // lps[0] is always 0
+
+        // the loop calculates lps[i] for i = 1 to M-1
+        int i = 1;
+        while (i < M) {
+            if (pat[i] == pat[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else // (pat[i] != pat[len])
+            {
+                // This is tricky. Consider the example.
+                // AAACAAAA and i = 7. The idea is similar
+                // to search step.
+                if (len != 0) {
+                    len = lps[len - 1];
+
+                    // Also, note that we do not increment
+                    // i here
+                }
+                else // if (len == 0)
+                {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+
+    // Prints occurrences of txt[] in pat[]
+    int KMPSearch(const char* pat, const char* txt)
+    {
+        int M = strlen(pat);
+        int N = strlen(txt);
+
+        // create lps[] that will hold the longest prefix suffix
+        // values for pattern
+        int lps[M];
+
+        // Preprocess the pattern (calculate lps[] array)
+        computeLPSArray(pat, M, lps);
+
+        int firstIndex = -1;
+        int i = 0; // index for txt[]
+        int j = 0; // index for pat[]
+        while (i < N) {
+            if (pat[j] == txt[i]) {
+                j++;
+                i++;
+            }
+
+            if (j == M) {
+                if(firstIndex == -1) {
+                    firstIndex = i - j;
+                }
+                j = lps[j - 1];
+            }
+
+            // mismatch after j matches
+            else if (i < N && pat[j] != txt[i]) {
+                // Do not match lps[0..lps[j-1]] characters,
+                // they will match anyway
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i = i + 1;
+            }
+        }
+        return firstIndex;
+    }
+public:
+    /*
+     * @param source: A source string
+     * @param target: A target string
+     * @return: An integer as index
+     */
+    int strStr2(const char* source, const char* target) {
+        if(source == nullptr || target == nullptr) return -1;
+        int sourceS = strlen(source);
+        int targetS = strlen(target);
+        if(targetS == 0) return 0;
+
+        return KMPSearch(target, source);
     }
 };
