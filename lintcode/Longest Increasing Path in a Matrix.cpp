f68473688791df412a1bd9903588dad0494da5d6
glc12125 <Liangchuan Gu>
2018-12-08 21:11:14 +0000

    Update

--- lintcode/Longest Increasing Path in a Matrix.cpp
+++ lintcode/Longest Increasing Path in a Matrix.cpp
@@ -1,35 +1,30 @@
+// DFS, memoisation search, there is no need to record visited position, because we move in
+// increasing direction
 class Solution {
 private:
     int m_xMoves[4] = {-1, 0, 1, 0};
     int m_yMoves[4] = {0, 1, 0, -1};
-    int m_maxLen = 1;
 
     bool isValidMove(int x, int y, int rowSize, int colSize) {
         return (0 <= x && x < rowSize && 0 <= y && y < colSize);
     }
 
-    int getKey(int x, int y, int colSize) {
-        return x * colSize + y;
-    }
+    int dfs(const vector<vector<int>> &matrix, int x, int y, vector<vector<int>>& mem) {
 
-    void dfs(const vector<vector<int>> &matrix, int x, int y, int currLen, unordered_set<int>& visited) {
-
-        m_maxLen = std::max(m_maxLen, currLen);
+        if(mem[x][y] != 0) return mem[x][y];
 
         int rowSize = matrix.size();
         int colSize = matrix[0].size();
-        int currKey = getKey(x, y, colSize);
 
         for(int k = 0; k < 4; ++k) {
             int nextX = x + m_xMoves[k];
             int nextY = y + m_yMoves[k];
             if(!isValidMove(nextX, nextY, rowSize, colSize)) continue;
-            int key = getKey(nextX, nextY, colSize);
-            if(visited.count(key)) continue;
             if(matrix[nextX][nextY] <= matrix[x][y]) continue;
-            dfs(matrix, nextX, nextY, currLen + 1, visited);
+            mem[x][y] = std::max(mem[x][y], dfs(matrix, nextX, nextY, mem));
         }
-        visited.erase(currKey);
+        mem[x][y] += 1;
+        return mem[x][y];
     }
 public:
     /**
@@ -42,12 +37,13 @@ public:
         int colSize = matrix[0].size();
         if(colSize == 0) return 0;
 
-        unordered_set<int> visited;
+        vector<vector<int>> mem(rowSize, vector<int>(colSize, 0));
+        int maxLen = 0;
         for(int i = 0; i < rowSize; ++i) {
             for(int j = 0; j < colSize; ++j) {
-                dfs(matrix, i, j, 1, visited);
+                maxLen = std::max(maxLen, dfs(matrix, i, j, mem));
             }
         }
-        return m_maxLen;
+        return maxLen;
     }
 };
