3d2539adfd9557d45ebbe52d499fe8e1ed992098
glc12125 <Liangchuan Gu>
2018-12-27 16:12:38 +0000

    Update

--- lintcode/Coin Path.cpp
+++ lintcode/Coin Path.cpp
@@ -52,3 +52,44 @@ public:
         return result;
     }
 };
+
+// From start to end
+class Solution {
+public:
+    /**
+     * @param A: a list of integer
+     * @param B: an integer
+     * @return: return a list of integer
+     */
+    vector<int> cheapestJump(vector<int> &A, int B) {
+        int len = A.size();
+        if(len == 1 && A[0] != -1) return {1};
+        if(A[0] == -1 || A[len-1] == -1) return {};
+
+        vector<int> res;
+        vector<int> dp(len, INT_MAX);
+        vector<int> path(len, -1);
+        // This means the hops from start to index i, the idea is that,
+        // the bigger hops, the smaller indexes from the beginning
+        vector<int> pathLen(len, 0);
+        dp[0] = 0;
+        for (int i = 0; i < len; ++i) {
+            if (A[i] == -1) continue;
+            for (int j = max(0, i - B); j < i; ++j) {
+                if (dp[j] == INT_MAX) continue;
+                int t = A[i] + dp[j];
+                if (t < dp[i] || (t == dp[i] && pathLen[i] < pathLen[j] + 1)) {
+                    dp[i] = t;
+                    path[i] = j;
+                    pathLen[i] = pathLen[j] + 1;
+                }
+            }
+        }
+        if (dp[len - 1] == INT_MAX) return res;
+        for (int cur = len - 1; cur != -1; cur = path[cur]) {
+            res.push_back(cur + 1);
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
