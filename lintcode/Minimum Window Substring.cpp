f04eb51cb5a229eafc56d8d92cdbcb62b574183a
glc12125 <Liangchuan Gu>
2018-12-28 12:47:35 +0000

    Update

--- lintcode/Minimum Window Substring.cpp
+++ lintcode/Minimum Window Substring.cpp
@@ -52,3 +52,39 @@ public:
         return result;
     }
 };
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int> mp;
+        for (char now : t) {
+            mp[now] ++;
+        }
+        int count = mp.size();
+        int j = 0;
+        int ans = INT_MAX;
+        string res;
+        for (int i = 0; i < s.size(); i++) {
+            while(count != 0 && j < s.size()) {
+                mp[s[j]]--;
+                if (mp[s[j]] == 0) {
+                    count--;
+                }
+                j++;
+                if (count == 0) {
+                    break;
+                }
+            }
+            if (count == 0 && j - i< ans) {
+                ans = j - i;
+                res = s.substr(i, j - i);
+
+            }
+            if(mp[s[i]] == 0) {
+                count++;
+            }
+            mp[s[i]]++;
+        }
+        return res;
+    }
+};
