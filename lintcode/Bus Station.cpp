df855d990e35f05ee030bcd2b488405b89f1132c
glc12125 <Liangchuan Gu>
2018-12-28 23:12:44 +0000

    Update

--- lintcode/Bus Station.cpp
+++ lintcode/Bus Station.cpp
@@ -62,3 +62,61 @@ public:
         return minSwitch == INT_MAX ? -1 : minSwitch;
     }
 };
+
+// Using stop in queue, it depends on how many bus numbers each route might have, because this solutions
+// pushes many stops into queue, could be slow when average bus numbers are huge.
+class Solution {
+public:
+    /**
+     * @param N: The number of buses
+     * @param route: The route of buses
+     * @param A: Start bus station
+     * @param B: End bus station
+     * @return: Return the minimum transfer number
+     */
+    int getMinTransferNumber(int N, vector<vector<int>> &route, int A, int B) {
+        if(A == B) return 0;
+        int len = route.size();
+        // Something is obviously wrong
+        if(N != len) return - 1;
+
+        unordered_map<int, unordered_set<int>> stopToRoute;
+        for(int i = 0; i < N; ++i) {
+            for(auto stop : route[i]) {
+                stopToRoute[stop].insert(i);
+            }
+        }
+
+        int minSwitch = 0;
+        queue<int> stopQ;
+        unordered_set<int> visitedRoute;
+        unordered_set<int> visitedStop;
+        stopQ.push(A);
+        visitedStop.insert(A);
+        const auto& BRoutes = stopToRoute[B];
+
+        while(!stopQ.empty()) {
+            ++minSwitch;
+            int len = stopQ.size();
+            while(len > 0) {
+                int currentStop = stopQ.front();
+                stopQ.pop();
+                for(auto nextRoute : stopToRoute[currentStop]) {
+                    if(visitedRoute.count(nextRoute)) continue;
+                    visitedRoute.insert(nextRoute);
+                    if(BRoutes.count(nextRoute)) {
+                        return minSwitch;
+                    }
+
+                    for(auto nextStop : route[nextRoute]) {
+                        if(visitedStop.count(nextStop)) {continue;}
+                        visitedStop.insert(nextStop);
+                        stopQ.push(nextStop);
+                    }
+                }
+                --len;
+            }
+        }
+        return -1;
+    }
+};
