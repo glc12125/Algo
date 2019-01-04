91c89c54b92793616c727dac25e7c02e32d416cb
glc12125 <Liangchuan Gu>
2019-01-04 23:29:50 +0000

    Update

--- lintcode/Maximum Sum of 3 Non-Overlapping Subarrays.cpp
+++ lintcode/Maximum Sum of 3 Non-Overlapping Subarrays.cpp
@@ -1,4 +1,4 @@
-// Prefix Sum, time O(N^3)
+// Naive direct Prefix Sum, time O(N^3)
 class Solution {
 public:
     /**
@@ -36,6 +36,54 @@ public:
                         }
                     }
                 }
+            }
+        }
+
+        return result;
+    }
+};
+
+// DP,
+
+class Solution {
+public:
+    /**
+     * @param nums: an array
+     * @param k: an integer
+     * @return: three non-overlapping subarrays with maximum sum
+     */
+    vector<int> maxSumOfThreeSubarrays(vector<int> &nums, int k) {
+        int len = nums.size();
+
+        vector<long long> prefixSum(len, 0);
+        prefixSum[0] = nums[0];
+        for(int i = 1; i < len; ++i) {
+            prefixSum[i] = prefixSum[i-1] + nums[i];
+            if(i >= k) {
+                prefixSum[i] -= nums[i-k];
+            }
+        }
+
+        vector<int> left(len, 0);
+        vector<int> right(len, 0);
+        left[k-1] = k - 1;
+        for(int i = k; i < len; ++i) {
+            left[i] = (prefixSum[i] > prefixSum[left[i-1]] ? i : left[i-1]);
+        }
+
+        right[len-1] = len - 1;
+        for(int i = len - 2; i >= 0; --i) {
+            right[i] = (prefixSum[i] > prefixSum[right[i+1]] ? i : right[i+1]);
+        }
+
+        long long maxSum = INT_MIN;
+        vector<int> result(3);
+
+        for(int i = 2 * k - 1; i <= len - k - 1; ++i) {
+            long long newSum = prefixSum[left[i-k]] + prefixSum[i] + prefixSum[right[i+k]];
+            if(newSum > maxSum) {
+                maxSum = newSum;
+                result = {left[i-k] - k + 1, i - k + 1, right[i+k] - k + 1};
             }
         }
 
