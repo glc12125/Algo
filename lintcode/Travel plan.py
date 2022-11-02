f69145db9d7a6d01463987a954dd787e6f49d420
Liangchuan Gu <glc12125@gmail.com>
2022-11-02 10:57:20 +0000

    travel plan

--- lintcode/Travel plan.py
+++ lintcode/Travel plan.py
@@ -14,26 +14,26 @@ class Solution:
             return 0
         self.min_distance = sys.maxsize
 
-        self.search(arr, 0, set([0]), 0, [])
+        self.search(arr, 0, set([0]), 0)
 
         return self.min_distance
 
-    def search(self, arr: List[List[int]], city: int, visited: Set, distance: int, path: List[int]):
-        if city == 0 and 1 < len(visited) < len(arr):
-            return
-        if len(visited) == len(arr) or len(path) > len(arr) + 1:
-            if city == 0 and distance < self.min_distance:
+    def search(self, arr: List[List[int]], city: int, visited: Set, distance: int):
+
+        if len(visited) == len(arr):
+            distance += arr[city][0]
+            if distance < self.min_distance:
                 self.min_distance = distance
             return
         if distance > self.min_distance:
             return
 
 
-        for next_city in range(0, len(arr[city])):
+        for next_city in range(1, len(arr[city])):
             if next_city == city:
                 continue
-            if next_city in visited and next_city != 0:
+            if next_city in visited:
                 continue
             visited.add(next_city)
-            self.search(arr, next_city, visited, distance + arr[city][next_city], path + [next_city])
+            self.search(arr, next_city, visited, distance + arr[city][next_city])
             visited.remove(next_city)
